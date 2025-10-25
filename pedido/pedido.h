80bd6d1ba172adbe667fa51ca1d77ca6d57c3a51
pradozl <gabrielprado.menezes@gmail.com>
2025-10-25 16:43:56 -0300

    Adicionei modulo pedido

---
+++ pedido/pedido.h
@@ -0,0 +1,31 @@
+#ifndef PEDIDO_H
+#define PEDIDO_H
+
+
+#define maxData 11
+#define maxStatus 20
+#define maxDescricao 100
+
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cliente.h"
+
+typedef struct {
+    int codigoPedido;
+    int codigoCliente;
+    char data[maxData];
+    char descricao[maxDescricao];
+    float valorTotal;
+    char status[maxStatus];
+}Pedido;
+
+int codigoPedidoJaExiste(FILE * fp, int code);
+void cadastrarPedido(FILE *fp);
+void listarPedidos(FILE *fp);
+void consultarPedido(FILE *fp, int code);
+FILE* deletarPedido(FILE* fp);
+void atualizarStatusPedido(FILE *fp, int code, const char *novoStatus);
+
+#endif
